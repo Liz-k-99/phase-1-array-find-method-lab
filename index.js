fa3ff5cbdadafd8946f20a1389e52073e16e496a
Elizabeth Kershaw <lizzykershaw99@hotmail.com>
2023-10-27 14:38:50 +1100

    find method

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+// record = [
+//     {year:'2014', result:'W',},
+//     {year:'2000', result:'L'},
+//     {year:'1999', result:'W'}
+// ]
+
+const superbowlWin = (records) => {
+    const winningRecord = records.find(record => record.result === 'W');
+    return winningRecord ? winningRecord.year : undefined;
+  };
+  
+//console.log(superbowlWin)
